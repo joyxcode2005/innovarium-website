daf25b20f6640018e0269a9a538c19001c0c0f2e
Joy Sengupta <joysengupta252005@gamil.com>
2025-04-25 00:41:51 +0530

    vite config fixed again

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(), 
+    react(),
     viteStaticCopy({
-      target:{
-        src: "public/_redirects",
-        dest: ".",
-      }
-    })
+      target: [
+        {
+          src: "public/_redirects",
+          dest: ".",
+        },
+      ],
+    }),
   ],
 });
